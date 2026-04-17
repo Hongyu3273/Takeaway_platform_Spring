211086a57c708749db6f99396a8087359b4b4033
Hongyu3273 <chenhongyu375@gmail.com>
2026-04-17 21:47:51 +1000

    admin order related functions

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -50,4 +50,10 @@ public interface OrderMapper {
 
 
 
+    /**
+     * 根据状态统计订单数量
+     * @param status
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
